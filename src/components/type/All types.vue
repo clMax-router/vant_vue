bd1c567588f2ad86ba133bcaa32734fff0b7678c
lineki <1840754797@qq.com>
2020-09-02 15:22:10 +0800

    修改

--- src/components/type/All types.vue
+++ src/components/type/All types.vue
@@ -1,48 +1,68 @@
 <template>
-    <van-tree-select height="100%" :items="items" :main-active-index.sync="active">
-      <template #content>
-        <van-grid :column-num="3" v-if="active === 0">
-          <van-grid-item
-            v-for="value in 9"
-            :key="value"
-            icon="photo-o"
-            text="文字"
-          />
-        </van-grid>
-        <van-grid :column-num="3" v-if="active === 1">
-          <van-grid-item
-            v-for="value in 9"
-            :key="value"
-            icon="photo-o"
-            text="数字"
-          />
-        </van-grid>
-        <van-grid :column-num="3" v-if="active === 2">
-          <van-grid-item
-            v-for="value in 9"
-            :key="value"
-            icon="photo-o"
-            text="1"
-          />
-        </van-grid>
-        <van-grid :column-num="3" v-if="active === 3">
-          <van-grid-item
-            v-for="value in 9"
-            :key="value"
-            icon="photo-o"
-            text="2"
-          />
-        </van-grid>
-        <van-grid :column-num="3" v-if="active === 4">
-          <van-grid-item
-            v-for="value in 9"
-            :key="value"
-            icon="photo-o"
-            text="3"
-          />
-        </van-grid>
-      </template>
-    </van-tree-select>
+  <van-tree-select
+    height="100%"
+    :items="items"
+    :main-active-index.sync="active"
+  >
+    <template #content>
+      <van-grid :column-num="3" v-if="active === 0">
+        <van-grid-item
+          v-for="(item, index) in imageList"
+          :icon="item.image"
+          :text="item.text"
+          :key="index"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 1">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="数字"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 2">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="1"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 3">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="2"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 4">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="3"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 5">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="4"
+        />
+      </van-grid>
+      <van-grid :column-num="3" v-if="active === 6">
+        <van-grid-item
+          v-for="value in 9"
+          :key="value"
+          icon="photo-o"
+          text="5"
+        />
+      </van-grid>
+    </template>
+  </van-tree-select>
 </template>
 
 <script>
@@ -50,8 +70,27 @@ export default {
   name: "allTypes",
   data() {
     return {
-        active: 0,
-        items: [{ text: '1' }, { text: '2' }, { text: '3' }, { text: '4' }, { text: '5' }],
+      active: 0,
+      items: [
+        { text: "1" },
+        { text: "2" },
+        { text: "3" },
+        { text: "4" },
+        { text: "5" },
+        { text: "6" },
+        { text: "7" },
+      ],
+      imageList: [
+        { image: require("../../assets/img/img1.jpg"), text: "1" },
+        { image: require("../../assets/img/img2.jpg"), text: "1" },
+        { image: require("../../assets/img/img3.jpg"), text: "1" },
+        { image: require("../../assets/img/img3.jpg"), text: "1" },
+        { image: require("../../assets/img/img2.jpg"), text: "1" },
+        { image: require("../../assets/img/img1.jpg"), text: "1" },
+        { image: require("../../assets/img/img1.jpg"), text: "1" },
+        { image: require("../../assets/img/img2.jpg"), text: "1" },
+        { image: require("../../assets/img/img3.jpg"), text: "1" },
+      ],
     };
   },
 
@@ -63,7 +102,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.van-sidebar-item--select{
-    border-color:#409eff;
+.van-sidebar-item--select {
+  border-color: #409eff;
 }
 </style>
