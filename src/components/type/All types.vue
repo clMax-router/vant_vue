22ceb9219a9fbfc1573d1d3027c3ff1766a95918
lineki <1840754797@qq.com>
2020-09-02 11:45:42 +0800

    修改

--- src/components/type/All types.vue
+++ src/components/type/All types.vue
@@ -1,48 +1,48 @@
 <template>
-    <div class="sidebar">
-        <van-sidebar v-model="activeKey" @change="onChange">
-            <van-sidebar-item to="/type/Game Category" title="游戏分类"/>
-            <van-sidebar-item to="/type/Home life" title="居家生活"/>
-            <van-sidebar-item to="/type/Hand-made model" title="手办模型"/>
-            <van-sidebar-item to="/type/Digital peripherals" title="数码外设"/>
-            <van-sidebar-item to="/type/Accessories pendant" title="配饰挂件"/>
-            <van-sidebar-item to="/type/Clothing" title="服装服饰"/>
-            <van-sidebar-item to="/type/Book stationery" title="图书文具"/>
-        </van-sidebar>
-    </div>
+  <div class="sidebar">
+    <van-tree-select :items="items" :main-active-index.sync="active">
+      <template #content>
+        <van-grid :column-num="3" v-if="active === 0">
+          <van-grid-item
+            v-for="value in 9"
+            :key="value"
+            icon="photo-o"
+            text="文字"
+          />
+        </van-grid>
+        <van-grid :column-num="3" v-if="active === 1">
+          <van-grid-item
+            v-for="value in 9"
+            :key="value"
+            icon="photo-o"
+            text="数字"
+          />
+        </van-grid>
+      </template>
+    </van-tree-select>
+  </div>
 </template>
 
 <script>
-    var routerArr = ["/type/Game Category","/type/Home life","/type/Hand-made model","/type/Digital peripherals","/type/Accessories pendant","/type/Clothing","/type/Book stationery"];
-    import { Notify } from 'vant';
-    export default {
-        name: "All types",
-        data() {
-            return {
-                activeKey: 0,
-            };
-        },
-        methods: {
-            onChange(index) {
-                Notify({ type: 'primary', message: index });
-            },
-        },
-        created() {
-            var path = this.$route.path;
-            this.index = routerArr.indexOf(path);
-        }
-    }
+export default {
+  name: "All types",
+  data() {
+    return {
+        active: 0,
+        items: [{ text: '1' }, { text: '2' }, { text: '3' }, { text: '4' }, { text: '5' }],
+    };
+  },
+
+  created() {
+    var path = this.$route.path;
+    this.index = routerArr.indexOf(path);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .sidebar{
-        width: 110px;
-        .van-sidebar{
-            width: 110px;
-            .van-sidebar-item--select{
-                color: #4aadf8;
-                border-color: #4aadf8;
-            }
-        }
-    }
+.sidebar{
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/views/Type/index.vue
+++ src/views/Type/index.vue
@@ -1,24 +1,20 @@
 <template>
-    <div class="main">
-            <ZNav/>
-            <All/>
-    </div>
+  <div class="main">
+    <ZNav />
+    <All />
+  </div>
 </template>
 
 <script>
 import ZNav from "@/components/nav";
-import All from "@/components/type/All types"
+import All from "@/components/type/All types";
 export default {
-    name:'type',
-    components: {
-        ZNav,
-        All
-    }
-}
+  name: "type",
+  components: {
+    ZNav,
+    All,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-   .main{
-       
-   }
-</style>
+<style lang="scss" scoped></style>
